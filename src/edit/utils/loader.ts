8bbaf50f02ef0bf7deae38935952194a99b90040
alijianzhang <zhangyl1998@126.com>
2024-09-26 11:32:38 +0800

    add DracoLoader

--- src/edit/utils/loader.ts
+++ src/edit/utils/loader.ts
@@ -1,16 +1,25 @@
+import * as THREE from "three";
 import { GLTFLoader, GLTF } from "three/addons/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 
 let gltfLoader: null | GLTFLoader = null;
+let dracoLoader: null | DRACOLoader = null;
 
 export interface ILoadResult {
   success: boolean;
   gltf?: GLTF;
   error?: any;
+  texture?: THREE.Texture;
 }
 
 export function loadGLTF(url: string, callback: (result: ILoadResult) => void) {
   if (!gltfLoader) {
     gltfLoader = new GLTFLoader();
+    //Set DracoLoader
+    dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath("draco/");
+    dracoLoader.setDecoderConfig({ type: "js" });
+    gltfLoader.setDRACOLoader(dracoLoader);
   }
   try {
     gltfLoader.load(url, (gltf) =>
