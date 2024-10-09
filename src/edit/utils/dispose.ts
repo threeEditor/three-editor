1a2f2e103c97832a4ca4ec431afc5f1cab9f8836
alijianzhang <zhangyl1998@126.com>
2024-10-09 10:57:53 +0800

    feat: hotupdate fix

--- src/edit/utils/dispose.ts
+++ src/edit/utils/dispose.ts
@@ -1,21 +1,28 @@
 import { Material, Mesh, Object3D } from "three";
 
 export function removeAllChild(node: Object3D) {
-    if(!node.children) {
-      if(node.parent) {
-        node.parent.remove(node);
-        if(node instanceof Mesh) {
-          node.geometry.dispose();
-          if(node.material instanceof Material) {
-            node.material.dispose();
-          } else if(Array.isArray(node.material)) {
-            node.material.forEach((material) => material.dispose());
-          }
+  const children = node?.children;
+  if (children?.length > 0) {
+    // // 递归删除子节点
+    children.forEach((child) => {
+      removeAllChild(child);
+    });
+  } else {
+    // // 删除当前节点（从叶子节点到根节点）
+    if (node.parent) {
+      //   // 先删除当前节点
+      node.parent.remove(node);
+      console.log("删除当前节点", node);
+      // 释放 geometry 和 material 资源
+      if (node instanceof Mesh) {
+        node.geometry.dispose();
+
+        if (node.material instanceof Material) {
+          node.material.dispose();
+        } else if (Array.isArray(node.material)) {
+          node.material.forEach((material) => material.dispose());
         }
       }
-    } else {
-      node.children.forEach(child => {
-        removeAllChild(child);
-      })
-    }
+    }  
   }
+}  
