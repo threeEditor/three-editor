247d6f61517bd149f44b32e01b5e935c85ab7654
Alei <zhangyl19980602@gmail.com>
2025-01-13 11:12:51 +0800

    feature viewhelper click event

--- src/edit/viewHelper/index.ts
+++ src/edit/viewHelper/index.ts
@@ -15,21 +15,38 @@ import {
   Sprite,
   Raycaster,
   Vector2,
+  Quaternion,
+  Euler,
+  Object3D,
+  Clock,
 } from 'three';
 
 // 右上角的视角帮助器 用于展示当前场景的视角
 export default class ViewHelper {
   private camera: PerspectiveCamera;
+  private domElement: HTMLElement | null = null;
+  private mouse: Vector2 = new Vector2();
+  private raycaster: Raycaster = new Raycaster();
+
   private viewHelperScene: Scene;
   private viewHelperCamera: OrthographicCamera;
   private viewHelperRenderer: WebGLRenderer;
 
+  private interactiveObjects: Sprite[];
+
+  private animating: boolean = false;
+  private q1: Quaternion = new Quaternion();
+  private q2: Quaternion = new Quaternion();
+  private center: Vector3 = new Vector3();
+  private targetQuaternion: Quaternion = new Quaternion();
+  private radius: number = 0;
+
+  private clock: Clock = new Clock();
+
   constructor(camera: PerspectiveCamera) {
     this.camera = camera;
 
-    const interactiveObjects = [];
-    const raycaster = new Raycaster();
-    const mouse = new Vector2();
+    this.interactiveObjects = [];
 
     const color1 = new Color('#ff4466');
     const color2 = new Color('#88ff44');
@@ -41,8 +58,6 @@ export default class ViewHelper {
     this.viewHelperCamera = new OrthographicCamera(-2, 2, 2, -2, 0, 4);
     this.viewHelperCamera.position.set(1, 1, 1);
     this.viewHelperCamera.lookAt(new Vector3(0, 0, 0));
-
-    const AxisGroup = new Group();
 
     const geometry = new CylinderGeometry(0.04, 0.04, 0.8, 5)
       .rotateZ(-Math.PI / 2)
@@ -88,6 +103,13 @@ export default class ViewHelper {
     negYAxisHelper.userData.type = 'negY';
     negZAxisHelper.userData.type = 'negZ';
 
+    this.interactiveObjects.push(posXAxisHelper);
+    this.interactiveObjects.push(posYAxisHelper);
+    this.interactiveObjects.push(posZAxisHelper);
+    this.interactiveObjects.push(negXAxisHelper);
+    this.interactiveObjects.push(negYAxisHelper);
+    this.interactiveObjects.push(negZAxisHelper);
+
     this.viewHelperScene.add(posXAxisHelper);
     this.viewHelperScene.add(posYAxisHelper);
     this.viewHelperScene.add(posZAxisHelper);
@@ -105,6 +127,15 @@ export default class ViewHelper {
     const viewportDom = document.getElementById('viewport_container');
     if (viewportDom) {
       viewportDom.appendChild(this.viewHelperRenderer.domElement);
+      this.domElement = viewportDom;
+
+      this.domElement.addEventListener('pointerup', (event) => {
+        event.stopPropagation();
+        this.handleClick(event);
+      });
+      this.domElement.addEventListener('pointerdown', (event) => {
+        event.stopPropagation();
+      });
     }
   }
   getAxisMaterial(color: Color) {
@@ -144,6 +175,118 @@ export default class ViewHelper {
     return new SpriteMaterial({ map: texture, toneMapped: false });
   }
 
+  // 捕获点击事件
+  handleClick(event: MouseEvent) {
+    const domElement = this.viewHelperRenderer.domElement;
+    if (!domElement) return;
+    const dim = 128;
+
+    const rect = domElement.getBoundingClientRect();
+    const offsetX = rect.left + (domElement.offsetWidth - dim);
+    const offsetY = rect.top + (domElement.offsetHeight - dim);
+    this.mouse.x = ((event.clientX - offsetX) / (rect.right - offsetX)) * 2 - 1;
+    this.mouse.y =
+      -((event.clientY - offsetY) / (rect.bottom - offsetY)) * 2 + 1;
+
+    this.raycaster.setFromCamera(this.mouse, this.viewHelperCamera);
+
+    const intersects = this.raycaster.intersectObjects(this.interactiveObjects);
+
+    if (intersects.length > 0) {
+      const intersection = intersects[0];
+      const object = intersection.object;
+      this.prepareAnimationData(object, this.center);
+
+      this.animating = true;
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+  prepareAnimationData(object: Object3D, focusPoint: Vector3) {
+    const targetPosition = new Vector3();
+    const targetQuaternion = new Quaternion();
+    const dummy = new Object3D();
+    const q1 = new Quaternion();
+    const q2 = new Quaternion();
+
+    switch (object.userData.type) {
+      case 'posX':
+        targetPosition.set(1, 0, 0);
+        targetQuaternion.setFromEuler(new Euler(0, Math.PI * 0.5, 0));
+        break;
+
+      case 'posY':
+        targetPosition.set(0, 1, 0);
+        targetQuaternion.setFromEuler(new Euler(-Math.PI * 0.5, 0, 0));
+        break;
+
+      case 'posZ':
+        targetPosition.set(0, 0, 1);
+        targetQuaternion.setFromEuler(new Euler());
+        break;
+
+      case 'negX':
+        targetPosition.set(-1, 0, 0);
+        targetQuaternion.setFromEuler(new Euler(0, -Math.PI * 0.5, 0));
+        break;
+
+      case 'negY':
+        targetPosition.set(0, -1, 0);
+        targetQuaternion.setFromEuler(new Euler(Math.PI * 0.5, 0, 0));
+        break;
+
+      case 'negZ':
+        targetPosition.set(0, 0, -1);
+        targetQuaternion.setFromEuler(new Euler(0, Math.PI, 0));
+        break;
+
+      default:
+        console.error('ViewHelper: Invalid axis.');
+    }
+    this.targetQuaternion = targetQuaternion;
+
+    //
+
+    const radius = this.camera.position.distanceTo(focusPoint);
+    this.radius = radius;
+    targetPosition.multiplyScalar(radius).add(focusPoint);
+
+    dummy.position.copy(focusPoint);
+
+    dummy.lookAt(this.camera.position);
+    q1.copy(dummy.quaternion);
+
+    dummy.lookAt(targetPosition);
+    q2.copy(dummy.quaternion);
+
+    this.q1 = q1;
+    this.q2 = q2;
+  }
+  public cameraUpdate(delta: number) {
+    const turnRate = 2 * Math.PI; // turn rate in angles per second
+    const step = delta * turnRate;
+    const { q1, q2, radius, targetQuaternion, camera } = this;
+
+    // animate position by doing a slerp and then scaling the position on the unit sphere
+
+    q1.rotateTowards(q2, step);
+    camera.position
+      .set(0, 0, 1)
+      .applyQuaternion(q1)
+      .multiplyScalar(radius)
+      .add(this.center);
+
+    // animate orientation
+
+    camera.quaternion.rotateTowards(targetQuaternion, step);
+
+    if (q1.angleTo(q2) === 0) {
+      this.animating = false;
+    }
+  }
+
   // 更新视角帮助器的渲染
   public update() {
     // copy editor camera position
@@ -154,5 +297,9 @@ export default class ViewHelper {
     );
     this.viewHelperCamera.lookAt(new Vector3(0, 0, 0));
     this.viewHelperRenderer.render(this.viewHelperScene, this.viewHelperCamera);
+    const delta = this.clock.getDelta();
+    if (this.animating === true) {
+      this.cameraUpdate(delta);
+    }
   }
 }
